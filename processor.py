ab69ae2c3760815ca75ff67f38b4eee0cdd201d6
Shivam Kalra <56111502+shivamkalra111@users.noreply.github.com>
2023-08-30 15:58:49 +0530

    Create processor.py

---
+++ processor.py
@@ -0,0 +1,13 @@
+from datetime import datetime,timedelta
+import pandas as pd
+
+
+
+
+def execute(req):
+    
+    start_date=req["start_time"]
+    end_date=req["end_time"]
+    
+
+    return start_date
